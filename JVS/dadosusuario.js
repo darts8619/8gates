a7e3258b39e3ec1a00a022e36329e6057c03151b
darts8619 <33968720+darts8619@users.noreply.github.com>
2017-11-24 22:23:19 +0000

    Add files via upload

---
+++ JVS/dadosusuario.js
@@ -0,0 +1,13 @@
+
+var usuario = {nome = "John", idade = "25",Unome= "Smith"}
+var i;
+
+document.getElementById("user1").text;
+
+if(usuario == user1){
+
+	document.write(usuario.nome);
+	document.write(usuario.idade);
+	document.write(usuario.Unome);
+
+}
